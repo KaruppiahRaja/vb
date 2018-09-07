f9effdfac589a36999daba96698ae2d52a8df039
KaruppiahRaja <karuppiah55555@gmail.com>
2018-09-07 10:52:45 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.Panel2 = New System.Windows.Forms.Panel
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(342, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(147, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Poor Richard", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(103, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 22)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter the key"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Add", "containskey", "containsvalue", "getbyindex", "getkey", "index of key", "index of value", "remove", "removeat", "clear", "display"})
+        Me.ComboBox1.Location = New System.Drawing.Point(342, 113)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(147, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poor Richard", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(152, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 22)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "options"
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(276, 216)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Poor Richard", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(103, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(133, 22)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Enter the value"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(342, 69)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(147, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(49, 274)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(491, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.LightCoral
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.TextBox4)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(62, 57)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(569, 332)
+        Me.Panel1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Poor Richard", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(45, 240)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 22)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Result"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Poor Richard", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(119, 165)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(117, 22)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Enter the key"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(342, 165)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(147, 20)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Snow
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Location = New System.Drawing.Point(62, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(569, 39)
+        Me.Panel2.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Poor Richard", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(230, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(97, 22)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Sorted List"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(662, 401)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,119 @@
+﻿Public Class Form1
+    Dim sl As SortedList = New SortedList()
+    Dim oj As Object
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If ComboBox1.SelectedIndex = 0 Then
+            sl.Add(TextBox1.Text, TextBox2.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            TextBox3.Text = sl.ContainsKey(TextBox4.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            TextBox3.Text = sl.ContainsValue(TextBox4.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            TextBox3.Text = sl.GetByIndex(TextBox4.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 4 Then
+            TextBox3.Text = sl.GetKey(TextBox4.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 5 Then
+            TextBox3.Text = sl.IndexOfKey(TextBox4.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 6 Then
+            TextBox3.Text = sl.IndexOfValue(TextBox4.Text)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 7 Then
+            oj = TextBox4.Text
+            sl.Remove(oj)
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            MsgBox("sorted list element cleared")
+        ElseIf ComboBox1.SelectedIndex = 8 Then
+            oj = TextBox4.Text
+            sl.RemoveAt(oj)
+            MsgBox("sorted list element cleared")
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 9 Then
+            sl.Clear()
+            MsgBox("sorted list cleared")
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        ElseIf ComboBox1.SelectedIndex = 10 Then
+            oj = sl.Count - 1
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            For i = 0 To oj
+                TextBox3.Text = TextBox3.Text + "  " + sl.GetKey(i) + ":" + sl.GetByIndex(i)
+
+            Next
+        End If
+    End Sub
+ 
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex = 1 Then
+            Label5.Visible = True
+            Label5.Text = "enter the key value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 0 Then
+            TextBox4.Visible = False
+            Label5.Visible = False
+        ElseIf ComboBox1.SelectedIndex = 10 Then
+            TextBox4.Visible = False
+            Label5.Visible = False
+        ElseIf ComboBox1.SelectedIndex = 9 Then
+            TextBox4.Visible = False
+            Label5.Visible = False
+
+
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            Label5.Visible = True
+            Label5.Text = "enter the  value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            Label5.Visible = True
+            Label5.Text = "enter the  index value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 4 Then
+            Label5.Visible = True
+            Label5.Text = "enter the  index value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 5 Then
+            Label5.Visible = True
+            Label5.Text = "enter the  key value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 6 Then
+            Label5.Visible = True
+            Label5.Text = "enter the   value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 7 Then
+            Label5.Visible = True
+            Label5.Text = "enter the  key value"
+            TextBox4.Visible = True
+        ElseIf ComboBox1.SelectedIndex = 8 Then
+            Label5.Visible = True
+            Label5.Text = "enter the  index value"
+            TextBox4.Visible = True
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox4.Visible = False
+        Label5.Visible = False
+    End Sub
+End Class
